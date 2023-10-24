00b9e2104a32154fbff8e938ffd563580100159c
Annmarie SanSevero <sophia.kai.charis@gmail.com>
2023-10-24 17:52:02 -0500

    working now...almost finished

--- scripts/project.js
+++ scripts/project.js
@@ -1,59 +1,42 @@
-//import articles from "../aof.js";
-
-// function test(articleNumber) {
-//     switch (articleNumber) {
-//         case "1": // Ensure the comparison is a string
-//             const article = JSON.parse(articles);
-//             const string = article.articles[0].content;
-//             document.getElementById(articleNumber).innerHTML = string;
-//             break;
-//         default:
-//             break;
-//     }
-// }
+import test from "./test.js";
 
 
 
-var numArticles = document.querySelectorAll(".aof").length; 
 
-for (var i= 0; i < numArticles; i++ ) {
-    document.querySelectorAll(".aof")[i].addEventListener("click", articleClick);
+var selectedArticle = "";
+var articles = document.querySelectorAll(".aof"); 
+
+console.log('articles', articles)
+for (var i= 0; i < articles.length; i++ ) {
+    articles[i].addEventListener("click", articleClick);
     //var articleNumber = this.button;
 
 }
 
+document.getElementById('test-button').addEventListener('click', testArticle)
+
+function testArticle() {
+    var userAnswer = document.getElementById("articleInput");
+    test(selectedArticle, userAnswer.value)
+}
 
 
-function articleClick() {
-    alert("clicked");
+function articleClick(e) {
+    //xalert("clicked");
+    
 
-   // Create event listener
-    document.addEventListener('click', (e) =>
-    {
   // Retrieve id from clicked element
         let elementId = e.target.id;
-}
-);
-
-    // add image using the id
-    /*var img = document.createElement("img");
-    img.src = `/images/${elementId}.png`;
-    var src = document.getElementById("verse");
-    src.appendChild(img);
-*/
+        selectedArticle = elementId;
+        // add image using the id
+        var img = document.createElement("img");
+        img.src = `/images/article${elementId}.png`;
+        var src = document.getElementById("verse");
+        src.appendChild(img);
+        //listening for the test input
     
 
-    
-}
+};
 
 
-/* async getArticles Function using fetch()*/
-
-// const getArticles = async () => {
-//     //In the function, declare a const variable named response that awaits the built-in fetch method calling the temple data, absolute URL given in Step 2 above
-//     const response = await fetch("articles-of-faith.json");
-//     let articleList = await response.json();
-//     console.log(articleList);
-
-//}
-//articles.init();
+test(1);
--- scripts/test.js
+++ scripts/test.js
@@ -1,14 +1,30 @@
-function test(articleNumber) {
+/* async getArticles Function using fetch()*/
+
+const getArticles = async () => {
+    //In the function, declare a const variable named response that awaits the built-in fetch method calling the temple data, absolute URL given in Step 2 above
+    const response = await fetch("articles-of-faith.json");
+    let articleList = await response.json();
+    console.log(articleList);
+    return articleList;
+}
+
+
+async function test(articleNumber, userAnswer) {
+    const articles = await getArticles();
+    console.log(articles)
+
     switch (articleNumber) {
         case "1": // Ensure the comparison is a string
             // Get the text input
-
+             
             //compare with json content
-
-            //if matches display congratulations
-
-            //if doesn't match display try again
-
+            if (userAnswer === articles.articles[articleNumber - 1].content){
+                alert("congrats! 🥳");
+            }
+            else {
+                alert("keep trying 🤔");
+            };
+            
 
 
         case "2":
@@ -47,4 +63,4 @@ function test(articleNumber) {
 }
 
 
-//export default test.js; 
+export default test; 
