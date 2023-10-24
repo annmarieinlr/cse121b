3fc6bd67b08c1afce9ed35a10ba8ee7fc2077989
Annmarie SanSevero <sophia.kai.charis@gmail.com>
2023-10-24 11:09:56 -0500

    more code added to test.js

--- scripts/test.js
+++ scripts/test.js
@@ -1,10 +1,46 @@
 function test(articleNumber) {
     switch (articleNumber) {
         case "1": // Ensure the comparison is a string
-            const article = JSON.parse(articleList);
+            // Get the text input
+
+            //compare with json content
+
+            //if matches display congratulations
+
+            //if doesn't match display try again
+
+
+
+        case "2":
+            break;
+
+        case "3":
+            break;
+        case "4":
+            break;
+        case "5":
+            break;
+        case "6":
+            break;
+        case "7":
+            break;
+        case "8":
+            break;
+        case "9":
+            break;
+        case "10":
+            break;
+        case "11":
+            break;
+        case "12":
+            break;
+        case "13":
+            break;
+                                                                                                                                                   
+        /*const article = JSON.parse(articleList);
             const string = article.articles[0].content;
             document.getElementById(articleNumber).innerHTML = string;
-            break;
+            break;*/
         default:
             break;
     }
